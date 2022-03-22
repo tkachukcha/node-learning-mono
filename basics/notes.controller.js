fcf64c8753f99eb6007a5488482d43e02598079e
Dan Tkachuk <daniil.tkachuk@gmail.com>
2022-03-22 17:49:40 +0300

    Add prinNotes and chalk func

--- basics/notes.controller.js
+++ basics/notes.controller.js
@@ -1,10 +1,10 @@
 const fs = require("fs/promises");
 const path = require("path");
+const chalk = require("chalk");
 
 const notesPath = path.join(__dirname, "db.json");
 
 async function addNote(title) {
-
   const notes = await getNotes();
   const note = {
     title,
@@ -13,6 +13,7 @@ async function addNote(title) {
 
   notes.push(note);
   await fs.writeFile(notesPath, JSON.stringify(notes));
+  console.log(chalk.bgGreen("Note added"));
 }
 
 async function getNotes() {
@@ -20,7 +21,16 @@ async function getNotes() {
   return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
 }
 
+async function printNotes() {
+  const notes = await getNotes();
+  console.log(chalk.bgBlueBright("List of notes:"));
+  notes.forEach((note) => {
+    console.log(chalk.blue(note.title));
+  });
+}
+
 module.exports = {
   addNote,
   getNotes,
+  printNotes,
 };

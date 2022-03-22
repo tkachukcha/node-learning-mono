39a2a665f90ceee1340315cae70757f5e1c75751
Dan Tkachuk <daniil.tkachuk@gmail.com>
2022-03-22 19:02:20 +0300

    Add simple response

--- server/index.js
+++ server/index.js
@@ -4,9 +4,10 @@ const chalk = require("chalk");
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-
+  console.log("server");
+  res.end("hello from server");
 });
 
 server.listen(port, () => {
   console.log(chalk.green(`Server started listening on port ${port}`));
-})
+});

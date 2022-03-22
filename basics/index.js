2f79be7924b708885e9ba4f39193cad55f1ac096
Dan Tkachuk <daniil.tkachuk@gmail.com>
2022-03-22 17:20:49 +0300

    Add title to add command

--- basics/index.js
+++ basics/index.js
@@ -1,1 +1,29 @@
 const yargs = require("yargs");
+const pkg = require("./package.json");
+
+yargs.version(pkg.version);
+
+yargs.command({
+  command: "add",
+  describe: "Add new note to list",
+  builder: {
+    title: {
+      type: "string",
+      describe: "Note title",
+      demandOption: true,
+    },
+  },
+  handler({ title }) {
+    console.log("Add command:", title);
+  },
+});
+
+yargs.command({
+  command: "list",
+  describe: "Print all notes",
+  handler() {
+    console.log("List command");
+  },
+});
+
+yargs.parse();
